4fc3acb587f0e9b4b75b378706e863bd147bd041
Jorge Osses <jorgitoosses@gmail.com>
2022-04-28 00:09:00 -0300

    Se agrega los productos destacados al data_prod

--- src/components/data-productos.js
+++ src/components/data-productos.js
@@ -91,51 +91,147 @@ export const dataProductos = [
     },
     {
         id: 11,
-        name: "",
-        img: "",
-        title: "",
-        linkWapp: "",
-        category: ""
+        name: "STICK",
+        img: "https://res.cloudinary.com/dw87bfoul/image/upload/v1651031792/samples/ecommerce/image_28_yrx0jw.png",
+        title: "Palo Hockey",
+        category: "hockey",
+        linkWapp: "https://wa.me/5491172320149?text=Hola!%20Me%20interesa%20el%20Palo%20de%20Hockey",
+        destacado: true
     },
     {
         id: 12,
         name: "",
-        img: "",
-        title: "",
-        linkWapp: "",
-        category: ""
+        img: "https://res.cloudinary.com/dw87bfoul/image/upload/v1651031791/samples/ecommerce/image_29_szzkev.png",
+        title: "Tiraband",
+        category: "kinesio",
+        linkWapp: "https://wa.me/5491172320149?text=Hola!%20Me%20interesa%20el%20Tiraband",
+        destacado: true
     },
     {
         id: 13,
         name: "",
-        img: "",
-        title: "",
-        linkWapp: "",
-        category: ""
+        img: "https://res.cloudinary.com/dw87bfoul/image/upload/v1651031791/samples/ecommerce/image_30_j7icgj.png",
+        title: "Relax Ball",
+        category: "kinesio",
+        linkWapp: "https://wa.me/5491172320149?text=Hola!%20Me%20interesa%20las%20RelaxBall",
+        destacado: true
     },
     {
         id: 14,
-        name: "",
-        img: "",
-        title: "",
-        linkWapp: "",
-        category: ""
+        name: "MARFED",
+        img: "https://res.cloudinary.com/dw87bfoul/image/upload/v1651031786/samples/ecommerce/image_31_bwe14e.png",
+        title: "Gorra Silicona",
+        category: "natacion",
+        linkWapp: "https://wa.me/5491172320149?text=Hola!%20Me%20interesa%20las%20gorra%20Marfed",
+        destacado: true
     },
     {
         id: 15,
-        name: "",
-        img: "",
-        title: "",
-        linkWapp: "",
-        category: ""
+        name: "MARFED",
+        img: "https://res.cloudinary.com/dw87bfoul/image/upload/v1651031785/samples/ecommerce/image_32_ooc1kw.png",
+        title: "Antiparras Danubio",
+        category: "natacion",
+        linkWapp: "https://wa.me/5491172320149?text=Hola!%20Me%20interesa%20las%20antiparras%20Marfed",
+        destacado: true
     },
     {
         id: 16,
         name: "",
-        img: "",
-        title: "",
-        linkWapp: "",
-        category: ""
+        img: "https://res.cloudinary.com/dw87bfoul/image/upload/v1651031783/samples/ecommerce/image_33_zpsptc.png",
+        title: "Colchoneta Yoga",
+        category: "pilates-yoga",
+        linkWapp: "https://wa.me/5491172320149?text=Hola!%20Me%20interesa%20las%20colchonetas%20de%20yoga",
+        destacado: true
+    },
+    {
+        id: 17,
+        name: "",
+        img: "https://res.cloudinary.com/dw87bfoul/image/upload/v1651031783/samples/ecommerce/image_34_agef1k.png",
+        title: "Pelota Pilates",
+        category: "pilates-yoga",
+        linkWapp: "https://wa.me/5491172320149?text=Hola!%20Me%20interesa%20la%20pelota%20de%20pilates",
+        destacado: true
+    },
+    {
+        id: 18,
+        name: "DABBER",
+        img: "https://res.cloudinary.com/dw87bfoul/image/upload/v1651031782/samples/ecommerce/image_35_hfgbcr.png",
+        title: "Paleta Padel",
+        category: "tenis-padel-pingpong",
+        linkWapp: "https://wa.me/5491172320149?text=Hola!%20Me%20interesa%20la%20paleta%20de%20padel",
+        destacado: true
+    },
+    {
+        id: 19,
+        name: "",
+        img: "https://res.cloudinary.com/dw87bfoul/image/upload/v1651114359/samples/ecommerce/voleyball_usxql4.jpg",
+        title: "Pelota Voley",
+        category: "voley",
+        linkWapp: "https://wa.me/5491172320149?text=Hola!%20Me%20interesa%20la%20pelota%20de%20Voley",
+        destacado: true
+    },
+    {
+        id: 20,
+        name: "",
+        img: "https://res.cloudinary.com/dw87bfoul/image/upload/v1651031792/samples/ecommerce/image_37_ycjhwy.png",
+        title: "Red Voley",
+        category: "Voley",
+        linkWapp: "https://wa.me/5491172320149?text=Hola!%20Me%20interesa%20la%20Red%20de%20Voley",
+        destacado: true
+    },
+    {
+        id: 21,
+        name: "",
+        img: "https://res.cloudinary.com/dw87bfoul/image/upload/v1651031792/samples/ecommerce/image_38_qg1ooo.png",
+        title: "Bolso Core Bag",
+        category: "entrenamiento",
+        linkWapp: "https://wa.me/5491172320149?text=Hola!%20Me%20interesa%20el%20Bolso%20Core%20Bag",
+        destacado: true
+    },
+    {
+        id: 22,
+        name: "BEATBOSS",
+        img: "https://res.cloudinary.com/dw87bfoul/image/upload/v1651031791/samples/ecommerce/image_39_iqzemf.png",
+        title: "Cabezal Boxeo",
+        category: "boxeo",
+        linkWapp: "https://wa.me/5491172320149?text=Hola!%20Me%20interesa%20el%20cabezal%20de%20boxeo",
+        destacado: true
+    },
+    {
+        id: 23,
+        name: "",
+        img: "https://res.cloudinary.com/dw87bfoul/image/upload/v1651031791/samples/ecommerce/image_40_hfbpyt.png",
+        title: "Zig Zag Coordinación",
+        category: "educacion-fisica",
+        linkWapp: "https://wa.me/5491172320149?text=Hola!%20Me%20interesa%20las%20cintas%20zig%20zag",
+        destacado: true
+    },
+    {
+        id: 24,
+        name: "",
+        img: "https://res.cloudinary.com/dw87bfoul/image/upload/v1651031786/samples/ecommerce/image_41_nrjd26.png",
+        title: "Cono Flexible 20 cm",
+        category: "educacion-fisica",
+        linkWapp: "https://wa.me/5491172320149?text=Hola!%20Me%20interesa%20los%20conos%20flexibles",
+        destacado: true
+    },
+    {
+        id: 25,
+        name: "",
+        img: "https://res.cloudinary.com/dw87bfoul/image/upload/v1651031785/samples/ecommerce/image_42_tpsvjc.png",
+        title: "Mancuerna Body",
+        category: "gimnasio",
+        linkWapp: "https://wa.me/5491172320149?text=Hola!%20Me%20interesan%20las%20mancuernas%20de%20body",
+        destacado: true
+    },
+    {
+        id: 26,
+        name: "",
+        img: "https://res.cloudinary.com/dw87bfoul/image/upload/v1651031783/samples/ecommerce/image_43_g4frgo.png",
+        title: "Red Arco Fútbol 11",
+        category: "futbol",
+        linkWapp: "https://wa.me/5491172320149?text=Hola!%20Me%20interesa%20la%20red%20de%20arco",
+        destacado: true
     },
     
 ]
