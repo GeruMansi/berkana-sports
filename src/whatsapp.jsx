fb771e297de5dca74b2a311d89254edfd761ef70
Geru <mangeragu@gmail.com>
2022-05-16 17:04:45 -0300

    correccion atributo rel

--- src/whatsapp.jsx
+++ src/whatsapp.jsx
@@ -5,7 +5,7 @@ import { faWhatsapp } from '@fortawesome/free-brands-svg-icons'
 const Whatsapp = () => {
   return (
     <>
-    <a href="https://api.whatsapp.com/send?phone=5491172320149&text=Hola%20quisiera%20contactarme%20con%20ustedes!" class="float" target="_blank">
+    <a href="https://api.whatsapp.com/send?phone=5491172320149&text=Hola%20quisiera%20contactarme%20con%20ustedes!" class="float" target="_blank" rel="noreferrer">
     <FontAwesomeIcon className="icon my-float" icon={faWhatsapp}/>
     </a>
     </>
